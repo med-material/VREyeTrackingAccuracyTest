b054068efb14ddbfb0fb49172ff529bbbea2ba97
YoNeXia <yohann.neraud@viacesi.fr>
2019-10-14 11:14:53 +0200

    MainMenu: Add script to create edit hover effect
    HighlightText.cs permits change the default value of any field by "Edit" when your mouse is inside, and value entered when mouse is outside.
    Add an header in new scripts. This header is in comment and contains copyright informations.

---
+++ Assets/Scripts/Menu/HighlightText.cs
@@ -0,0 +1,38 @@
+﻿//
+// Copyright: Aalborg University
+// Author: YoNeXia <yohann.neraud@viacesi.fr> (https://gitlab.com/YoNeXia)
+// Git: https://github.com/med-material/VREyeTrackingAccuracyTest
+//
+// Date: 9/27/2019
+//
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class HighlightText : MonoBehaviour
+{
+
+    // Set variables
+    private Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get component
+        text = GetComponentInChildren<Text>();
+    }
+
+    // When mouse enter in the Input Field
+    public void MouseInInputField()
+    {
+        // Set text in iput Field
+        text.text = "Edit";
+    }
+
+    // When mouse exit the Input Field
+    public void MouseOutInputField()
+    {
+        // Set text in iput Field
+        text.text = "";
+    }
+}
