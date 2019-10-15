15155a0b82f10614ef6345c9183c8dab1dd96ffb
YoNeXia <yohann.neraud@viacesi.fr>
2019-10-15 09:08:11 +0200

    Add script to control font-size
    Add script to set the same size of font of all instructions part.

---
+++ Assets/Scripts/Menu/TextSetSize.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextSetSize : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text line_1, line_2;
+
+    void Update()
+    {
+        if (!line_1)
+        {
+            Debug.Log("No first line enter.");
+            return;
+        }
+        line_2.fontSize = line_1.cachedTextGenerator.fontSizeUsedForBestFit;
+        return;
+    }
+}
