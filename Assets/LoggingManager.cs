396f4bf0725476056a953e93b333c4978e83ae1a
YoNeXia <yohann.neraud@viacesi.fr>
2019-10-15 10:36:22 +0200

    Change linting in LoggingManager
    Re-indent all the LoggingManager.cs with automatic command in Visual Studio.
    Change all names of data to match with names in Unity.

--- Assets/LoggingManager.cs
+++ Assets/LoggingManager.cs
@@ -3,135 +3,154 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LoggingManager : MonoBehaviour {
+public class LoggingManager : MonoBehaviour
+{
 
-	private Dictionary<string, List<string>> metaCollection;
+    private Dictionary<string, List<string>> metaCollection;
 
-	private Dictionary<string, List<string>> logCollection;
+    private Dictionary<string, List<string>> logCollection;
 
-	private ConnectToMySQL mySQL;
+    private ConnectToMySQL mySQL;
 
-	[SerializeField]
-	private InputField emailInputField;
+    [SerializeField]
+    private InputField emailInputField;
 
-	[SerializeField]
-	private InputField participantInputField;
+    [SerializeField]
+    private InputField userInputField;
 
-	[SerializeField]
-	private InputField customConditionInputField;
+    [SerializeField]
+    private InputField commentInputField;
 
-	[SerializeField]
-	private Toggle spacebarInteractToggle;
+    [SerializeField]
+    private Toggle spacebarInteractToggle;
 
-	[SerializeField]
-	private Toggle showGrid;
+    [SerializeField]
+    private Toggle showGrid;
 
-	[SerializeField]
-	private Toggle showGazeDot;
+    [SerializeField]
+    private Toggle showGazeDot;
 
-	private bool hasLoggedCalibration = false;
+    private bool hasLoggedCalibration = false;
 
-	private float startTime = 0f;
+    private float startTime = 0f;
 
-	[SerializeField]
-	private StartConfig startConfig;
+    [SerializeField]
+    private StartConfig startConfig;
 
-	void Awake () {
-		logCollection = new Dictionary<string, List<string>>();
+    void Awake()
+    {
+        logCollection = new Dictionary<string, List<string>>();
 
-		// Add the database columns
-		logCollection.Add("Email", new List<string>());
-		logCollection.Add("DateAdded", new List<string>());
-		logCollection.Add("ParticipantNumber", new List<string>());
-		logCollection.Add("CircleName", new List<string>());
-		logCollection.Add("EyeTrackAccuracy", new List<string>());
-		logCollection.Add("CustomCondition", new List<string>());
-		logCollection.Add("ShowGazeDot", new List<string>());
-		logCollection.Add("ShowGrid", new List<string>());
-		logCollection.Add("SpacebarInteract", new List<string>());
+        // Add the database columns
+        logCollection.Add("Email", new List<string>());
+        logCollection.Add("DateAdded", new List<string>());
+        logCollection.Add("UserID", new List<string>());
+        logCollection.Add("CircleName", new List<string>());
+        logCollection.Add("EyeTrackAccuracy", new List<string>());
+        logCollection.Add("Comment", new List<string>());
+        logCollection.Add("ShowGazeDot", new List<string>());
+        logCollection.Add("ShowGrid", new List<string>());
+        logCollection.Add("SpacebarInteract", new List<string>());
 
-		metaCollection = new Dictionary<string, List<string>>();
-		metaCollection.Add("Email", new List<string>());
-		metaCollection.Add("DateAdded", new List<string>());
-		metaCollection.Add("ParticipantNumber", new List<string>());
-		metaCollection.Add("CircleName", new List<string>());
-		metaCollection.Add("EyeTrackAccuracy", new List<string>());
-		metaCollection.Add("CustomCondition", new List<string>());
-		metaCollection.Add("ShowGazeDot", new List<string>());
-		metaCollection.Add("ShowGrid", new List<string>());
-		metaCollection.Add("SpacebarInteract", new List<string>());
+        metaCollection = new Dictionary<string, List<string>>();
+        metaCollection.Add("Email", new List<string>());
+        metaCollection.Add("DateAdded", new List<string>());
+        metaCollection.Add("UserID", new List<string>());
+        metaCollection.Add("CircleName", new List<string>());
+        metaCollection.Add("EyeTrackAccuracy", new List<string>());
+        metaCollection.Add("Comment", new List<string>());
+        metaCollection.Add("ShowGazeDot", new List<string>());
+        metaCollection.Add("ShowGrid", new List<string>());
+        metaCollection.Add("SpacebarInteract", new List<string>());
 
-		mySQL = gameObject.GetComponent<ConnectToMySQL>();
-		
-		DontDestroyOnLoad(this.gameObject);
-	}
+        mySQL = gameObject.GetComponent<ConnectToMySQL>();
 
-	public void ToggleStartCalibration() {
-		if (string.IsNullOrEmpty(emailInputField.text)) {
-			return;
-		}
-		if (string.IsNullOrEmpty(participantInputField.text)) {
-			return;
-		}
+        DontDestroyOnLoad(this.gameObject);
+    }
 
-		if (!hasLoggedCalibration) {
-			startTime = 0f;
-			metaCollection["Email"].Add(emailInputField.text);
-			
-			metaCollection["ParticipantNumber"].Add(participantInputField.text);
+    public void ToggleStartCalibration()
+    {
+        if (string.IsNullOrEmpty(emailInputField.text) || string.IsNullOrEmpty(userInputField.text))
+        {
+            missingField.GetComponent<MissingField>().ChangeTextValue();
+            return;
+        }
 
-			if (showGazeDot.isOn) {
-			metaCollection["ShowGazeDot"].Add("On");
-			} else {
-			metaCollection["ShowGazeDot"].Add("Off");
-			}
+        if (!hasLoggedCalibration)
+        {
+            startTime = 0f;
+            metaCollection["Email"].Add(emailInputField.text);
 
-			if (showGrid.isOn) {
-			metaCollection["ShowGrid"].Add("On");
-			} else {
-			metaCollection["ShowGrid"].Add("Off");			
-			}
+            metaCollection["UserID"].Add(userInputField.text);
 
-			if (spacebarInteractToggle.isOn) {
-			metaCollection["SpacebarInteract"].Add("On");
-			} else {
-			metaCollection["SpacebarInteract"].Add("Off");
-			}
+            if (showGazeDot.isOn)
+            {
+                metaCollection["ShowGazeDot"].Add("On");
+            }
+            else
+            {
+                metaCollection["ShowGazeDot"].Add("Off");
+            }
 
-			if (string.IsNullOrEmpty(customConditionInputField.text)) {
-				metaCollection["CustomCondition"].Add("No Condition");				
-			} else {
-				metaCollection["CustomCondition"].Add(customConditionInputField.text);
-			}
+            if (showGrid.isOn)
+            {
+                metaCollection["ShowGrid"].Add("On");
+            }
+            else
+            {
+                metaCollection["ShowGrid"].Add("Off");
+            }
 
-			hasLoggedCalibration = true;
-		
-		}
-		startConfig.startCalibration();
-	}
+            if (spacebarInteractToggle.isOn)
+            {
+                metaCollection["SpacebarInteract"].Add("On");
+            }
+            else
+            {
+                metaCollection["SpacebarInteract"].Add("Off");
+            }
 
-	public void DuplicateMetaColumns() {
-		logCollection["Email"].Add(metaCollection["Email"][0]);
-		logCollection["ParticipantNumber"].Add(metaCollection["ParticipantNumber"][0]);
-		logCollection["ShowGazeDot"].Add(metaCollection["ShowGazeDot"][0]);
-		logCollection["ShowGrid"].Add(metaCollection["ShowGrid"][0]);
-		logCollection["SpacebarInteract"].Add(metaCollection["SpacebarInteract"][0]);
-		logCollection["CustomCondition"].Add(metaCollection["CustomCondition"][0]);	
-	}
+            if (string.IsNullOrEmpty(commentInputField.text))
+            {
+                metaCollection["Comment"].Add("No Condition");
+            }
+            else
+            {
+                metaCollection["Comment"].Add(commentInputField.text);
+            }
 
-	public void WriteToLog(string varName, string varValue) {
-		logCollection[varName].Add(varValue);
+            hasLoggedCalibration = true;
 
-	}
+        }
+        startConfig.startCalibration();
+    }
 
-	// TODO: Write to log file on disk as backup
+    public void DuplicateMetaColumns()
+    {
+        logCollection["Email"].Add(metaCollection["Email"][0]);
+        logCollection["UserID"].Add(metaCollection["UserID"][0]);
+        logCollection["ShowGazeDot"].Add(metaCollection["ShowGazeDot"][0]);
+        logCollection["ShowGrid"].Add(metaCollection["ShowGrid"][0]);
+        logCollection["SpacebarInteract"].Add(metaCollection["SpacebarInteract"][0]);
+        logCollection["Comment"].Add(metaCollection["Comment"][0]);
+    }
 
-	public void UploadLogs() {
-		mySQL.AddToUploadQueue(logCollection);
-		mySQL.UploadNow();
-		foreach (string key in logCollection.Keys) {
-			Debug.Log("Key: " + key + ", Count: " + logCollection[key].Count.ToString());
-			logCollection[key].Clear();
-		}
-	}
+    public void WriteToLog(string varName, string varValue)
+    {
+        logCollection[varName].Add(varValue);
+
+    }
+
+    // TODO: Write to log file on disk as backup
+
+    public void UploadLogs()
+    {
+        mySQL.AddToUploadQueue(logCollection);
+        mySQL.UploadNow();
+        foreach (string key in logCollection.Keys)
+        {
+            Debug.Log("Key: " + key + ", Count: " + logCollection[key].Count.ToString());
+            logCollection[key].Clear();
+        }
+    }
 }
