783283ad83db2f99e26161f3226bf6e12e02a66d
YoNeXia <yohann.neraud@viacesi.fr>
2019-10-15 10:38:44 +0200

    Add script to check empty field.
    Add new script to control text on left part of start button to inform the facilitator if he forget to complete any field.
    A swicth case control if the correct error message is wrote into the interface when function is called.
    LoggingManager.cs script updated to call function when empty field is detected.

---
+++ Assets/Scripts/Menu/MissingField.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissingField : MonoBehaviour
+{
+
+    private Text missingField;
+
+    // Use this for initialization
+    void Start()
+    {
+        missingField = GetComponent<Text>();
+    }
+
+    public void ChangeTextValue(int field)
+    {
+        switch (field)
+        {
+            case 1:
+                missingField.text = "Warning, mail field is empty!";
+                break;
+            case 2:
+                missingField.text = "Warning, user ID field is empty!";
+                break;
+            case 3:
+                missingField.text = "Warning, test number field is empty!";
+                break;
+            default:
+                break;
+
+        }
+
+    }
+}
