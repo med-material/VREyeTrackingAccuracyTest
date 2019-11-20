3748fa28a9f65cbb2435a5fef8e9276e14befac2
YoNeXia <yohann.neraud@viacesi.fr>
2019-11-20 14:42:15 +0100

    Add a button in Circle Test menu to load a scene
    New button added in the Circle Test menu to load a scene.
    You can choose this scene in the script link on the button. Just need to write the name of the scene want to be loaded.

---
+++ Assets/Scripts/CircleTest/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    [SerializeField]
+    private string name_scene;
+
+    public void LoadChosenScene()
+    {
+        SceneManager.LoadScene(name_scene);
+    }
+}
